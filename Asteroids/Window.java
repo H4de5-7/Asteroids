2516ca4494581a351fe3f789343b8b2feb27b2ab
H4de5-7 <754681612@qq.com>
2022-03-01 10:34:45 +0800

    Asteroids

---
+++ Asteroids/Window.java
@@ -0,0 +1,20 @@
+package Asteroids;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Window {
+    public static void main(String[] args) {
+        int Height=Windows.HEIGHT.getValue();
+        int Width=Windows.WIDTH.getValue();
+        JFrame window = new JFrame();
+        window.setSize(Width, Height);
+        window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        Picture pic = new Picture();
+        window.add(pic);
+        MeListener ll = new MeListener(pic);
+        window.addKeyListener(ll);
+        window.setVisible(true);
+        //pic.Music();
+        pic.action();
+    }
+}
---
+++ README.txt
@@ -0,0 +1,8 @@
+Asteroids game
+
+main class: Window
+up: speed up
+down: slow down
+left: turn left
+right: turn right
+shift: flash
